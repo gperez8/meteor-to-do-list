d083a4c7de8357f5ba034f29d60fed0096e3a296
gperez8 <gregory.facyt@gmail.com>
2017-11-17 12:38:45 -0400

    se corrigio error faltaba metodo render

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -12,10 +12,24 @@ class App extends Component {
             <Task key={task._id} task={task} />
         ));
     }
+
+    render() {
+        return (
+            <div className="container">
+                <header>
+                    <h1>Todo List</h1>
+                </header>
+         
+                <ul>
+                    {this.renderTasks()}
+                </ul>
+            </div>
+        );
+    }
 }
 
 App.propTypes = {
-    tasks: PropTypes.array.isRequired,
+    //tasks: PropTypes.array.isRequired,
 };
 
 export default createContainer(() => {
